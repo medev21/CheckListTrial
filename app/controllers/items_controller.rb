01c74c48a5c8736c464e62e20110ebb95560043f
Martin Ben <mebenav88@gmail.com>
2016-01-14 12:29:42 -0600

    add edit update destroy controllers and edit view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
+  end
+
+  def show
   end
 
   #this is where controls the new view
@@ -19,8 +23,28 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
     def item_params
       params.require(:item).permit(:title, :description)
+    end
+
+    def find_item
+      @item = Item.find(params[:id])
     end
 end
